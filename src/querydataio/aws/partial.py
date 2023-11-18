cd63bb3f3bbf04ab53a665484117d2d82649f32e
Ryan Caught <rcaught@gmail.com>
2023-11-18 16:30:11 +1000

    feat: move whats-new into run and have top level partial runner

---
+++ src/querydataio/aws/partial.py
@@ -0,0 +1,26 @@
+import sys
+
+from sqlite_utils import Database
+import querydataio.aws.whats_new.partial as whats_new
+
+from querydataio.aws import shared as aws_shared
+from querydataio import shared
+
+print()
+print("Partial download")
+print("================")
+print()
+
+sqlitedb = Database(aws_shared.SQLITE_DB)
+duckdb = shared.init_duckdb()
+
+print("AWS")
+print("===")
+
+if not all(x.run(print_indent=2) for x in [whats_new]):
+    print()
+    print("No new data... hard exit to stop deploy")
+    print("=======================================")
+    sys.exit(1)
+
+aws_shared.final_database_optimisations(sqlitedb)
