44da468809bbfe47384bec3ce61a899a275f54fa
Ryan Caught <rcaught@gmail.com>
2023-11-18 13:32:58 +1000

    feat: create a higher level runner for supporting multiple sources

---
+++ src/querydataio/aws/full.py
@@ -0,0 +1,20 @@
+from sqlite_utils import Database
+import querydataio.aws.whats_new.full as whats_new
+import querydataio.aws.blogs.full as blogs
+from querydataio.aws import shared as aws_shared
+from querydataio import shared
+
+print()
+print("Full download")
+print("=============")
+print()
+
+sqlitedb = Database(aws_shared.SQLITE_DB)
+duckdb = shared.init_duckdb()
+
+print("AWS")
+print("===")
+
+aws_shared.drop_tables([aws_shared.tags_table(sqlitedb)], print_indent=2)
+
+whats_new.run(sqlitedb, duckdb, print_indent=2)
--- src/querydataio/aws/whats_new/full.py
+++ src/querydataio/aws/whats_new/full.py
@@ -7,36 +7,39 @@ from querydataio import shared
 from querydataio.aws import shared as aws_shared
 from querydataio.aws import whats_new
 
-print()
-print("Full download")
-print()
 
-sqlitedb = Database(aws_shared.SQLITE_DB)
-duckdb = shared.init_duckdb()
+def run(sqlitedb: Database, duckdb: DuckDBPyConnection, print_indent=0):
+    print()
+    print(f"{print_indent * ' '}Whats New")
+    print(f"{print_indent * ' '}=========")
 
-main_table: Table = sqlitedb.table(whats_new.SQLITE_WHATS_NEW_TABLE_NAME)
-tags_table: Table = sqlitedb.table(whats_new.SQLITE_WHATS_NEW_TAGS_TABLE_NAME)
+    main_table: Table = sqlitedb.table(whats_new.SQLITE_WHATS_NEW_TABLE_NAME)
+    join_table: Table = sqlitedb.table(whats_new.SQLITE_WHATS_NEW_TAGS_TABLE_NAME)
 
-print()
-print("Dropping tables")
-print("===============")
+    aws_shared.drop_tables([main_table, join_table], print_indent=print_indent + 2)
 
-main_table.drop(ignore=True)
-print(f"{main_table.name}... done")
-tags_table.drop(ignore=True)
-print(f"{tags_table.name}... done")
+    downloaded = aws_shared.download(
+        duckdb,
+        whats_new.URL_PREFIX,
+        whats_new.TAG_ID_PREFIX,
+        range(2023, shared.current_year() + 1),
+        max_pages=1,
+        print_indent=print_indent + 2,
+    )
+    result_updates, result_tags, result_joins = whats_new.process(
+        duckdb, downloaded, print_indent=print_indent+2
+    )
 
-downloaded_years = aws_shared.download(
-    duckdb,
-    whats_new.URL_PREFIX,
-    whats_new.DIRECTORY_ID,
-    whats_new.TAG_ID_PREFIX,
-    range(whats_new.FIRST_YEAR, shared.current_year() + 1),
-)
-result_updates, result_tags = whats_new.process(duckdb, downloaded_years)
+    aws_shared.to_sqlite(
+        [
+            (result_updates, main_table),
+            (result_tags, aws_shared.tags_table(sqlitedb)),
+            (result_joins, join_table),
+        ],
+        print_indent=print_indent + 2,
+    )
 
-aws_shared.to_sqlite(main_table.name, result_updates)
-aws_shared.to_sqlite(tags_table.name, result_tags)
-
-whats_new.initial_sqlite_transform(main_table)
-whats_new.final_sqlite_transform(sqlitedb, main_table, tags_table)
+    whats_new.initial_sqlite_transform(main_table)
+    whats_new.final_sqlite_transform(
+        sqlitedb, main_table, aws_shared.tags_table(sqlitedb), join_table
+    )
