6036ccdbe6e8c37f032d9dfe2a1e02ae849cb444
Ryan Caught <rcaught@gmail.com>
2023-12-09 00:46:07 +1000

    feat: add stats extensions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,6 +11,7 @@ poetry run datasette publish vercel \
   --install=datasette-vega \
   --install=datasette-atom \
   --plugins-dir=plugins \
+  --load-extension=extensions/stats.so \
   --vercel-json=vercel.json \
   --setting default_page_size 100 \
   --setting allow_csv_stream off \
