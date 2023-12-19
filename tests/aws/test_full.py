26b2eeabb3554efbf16f07d0c09d485e487037c4
Ryan Caught <rcaught@gmail.com>
2023-12-20 02:24:20 +1000

    test: first integration test with one directory

---
+++ tests/aws/test_full.py
@@ -0,0 +1,61 @@
+from pytest_mock import MockerFixture
+from querydataio import test_utils
+from querydataio.aws import (
+    analyst_reports,
+    blog_posts,
+    executive_insights,
+    media_coverage,
+    products,
+    security_bulletins,
+    whats_new,
+)
+from querydataio.aws.full import FullRun
+import sqlite_utils
+import json
+
+
+def test_integration(mocker: MockerFixture) -> None:
+    test_utils.download_side_effect(
+        mocker,
+        {
+            "analyst_reports_total_hits": "tests/fixtures/aws/analyst_reports/download.1.json",
+            "analyst_reports": "tests/fixtures/aws/analyst_reports/download.2.json",
+        },
+    )
+
+    full_run = FullRun(
+        {
+            "database": "tests/dbs/aws.duckdb.db",
+            "config": {"disabled_filesystems": "HTTPFileSystem"},
+        },
+        {
+            # f"tests/dbs/aws_{whats_new.MAIN_TABLE_NAME}.sqlite3": [
+            #     {whats_new: whats_new.all_years()}
+            # ],
+            # f"tests/dbs/aws_{blog_posts.MAIN_TABLE_NAME}.sqlite3": [
+            #     {blog_posts: blog_posts.aws_categories()}
+            # ],
+            "tests/dbs/aws_general.sqlite3": [
+                {analyst_reports: []},
+                #     {executive_insights: []},
+                #     {media_coverage: []},
+                #     {products: []},
+                #     {security_bulletins: []},
+            ],
+        },
+    )
+
+    full_run.prepare()
+    full_run.run()
+
+    # print(have) to generate json file
+
+    with open("tests/fixtures/aws/analyst_reports/query.1.json", "r") as file:
+        want = json.dumps(json.load(file))
+        have = json.dumps(
+            sqlite_utils.Database(
+                "tests/dbs/aws_general.sqlite3"
+            ).execute_returning_dicts("SELECT * FROM analyst_reports ORDER BY id")
+        )
+
+        assert want == have
