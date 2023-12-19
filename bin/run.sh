07fb167581c4ae34b50d685c637f0e4dce82be12
Ryan Caught <rcaught@gmail.com>
2023-12-19 12:33:41 +1000

    chore: allow runner to work inside of dev container

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-docker run -p 8001:8001 -v `pwd`:/mnt \
-    datasetteproject/datasette \
-    datasette -p 8001 -h 0.0.0.0 \
-    -i /mnt/dbs/aws_general.sqlite3 \
-    -i /mnt/dbs/aws_blog_posts.sqlite3 \
-    -i /mnt/dbs/aws_whats_new.sqlite3 \
-    --metadata /mnt/metadata.yml --crossdb --reload \
-    --plugins-dir /mnt/plugins --load-extension=/mnt/extensions/stats.so
+mkdir -p plugins/datasette-updated/ && \
+echo '{
+  "plugins": {
+    "datasette-updated": {
+      "updated": "'"$(date -Iseconds)"'"
+    }
+  }
+}' > plugins/datasette-updated/metadata.json && \
+poetry run datasette package \
+  dbs/aws_general.sqlite3 \
+  dbs/aws_blog_posts.sqlite3 \
+  dbs/aws_whats_new.sqlite3 \
+  --tag ds \
+  --metadata metadata.yml \
+  --plugins-dir plugins \
+  --install=datasette-vega \
+  --install=datasette-atom \
+  --install=datasette-updated && \
+docker run -it -p 8081:8001 ds --crossdb
+
