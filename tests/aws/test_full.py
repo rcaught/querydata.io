65aba9d0ac788981fdbaa79e2bae0aae1b88bdcb
Ryan Caught <rcaught@gmail.com>
2023-12-21 21:13:05 +1000

    test: whats_new integration test

--- tests/aws/test_full.py
+++ tests/aws/test_full.py
@@ -18,6 +18,8 @@ def test_integration(mocker: MockerFixture) -> None:
     test_utils.download_side_effect(
         mocker,
         {
+            "whats_new_total_hits": "tests/fixtures/aws/whats_new/download.1.json",
+            "whats_new": "tests/fixtures/aws/whats_new/download.2.json",
             "analyst_reports_total_hits": "tests/fixtures/aws/analyst_reports/download.1.json",
             "analyst_reports": "tests/fixtures/aws/analyst_reports/download.2.json",
         },
@@ -29,9 +31,9 @@ def test_integration(mocker: MockerFixture) -> None:
             "config": {"disabled_filesystems": "HTTPFileSystem"},
         },
         {
-            # f"tests/dbs/aws_{whats_new.MAIN_TABLE_NAME}.sqlite3": [
-            #     {whats_new: whats_new.all_years()}
-            # ],
+            f"tests/dbs/aws_{whats_new.MAIN_TABLE_NAME}.sqlite3": [
+                {whats_new: whats_new.all_years()}
+            ],
             # f"tests/dbs/aws_{blog_posts.MAIN_TABLE_NAME}.sqlite3": [
             #     {blog_posts: blog_posts.aws_categories()}
             # ],
@@ -49,6 +51,16 @@ def test_integration(mocker: MockerFixture) -> None:
     full_run.run()
 
     # print(have) to generate json file
+
+    with open("tests/fixtures/aws/whats_new/query.1.json", "r") as file:
+        want = json.dumps(json.load(file))
+        have = json.dumps(
+            sqlite_utils.Database(
+                "tests/dbs/aws_whats_new.sqlite3"
+            ).execute_returning_dicts("SELECT * FROM whats_new ORDER BY id")
+        )
+
+        assert want == have
 
     with open("tests/fixtures/aws/analyst_reports/query.1.json", "r") as file:
         want = json.dumps(json.load(file))
