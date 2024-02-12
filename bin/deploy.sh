631db39885f0aa959cabd4fbf356aa0c8732cc01
Ryan Caught <rcaught@gmail.com>
2024-02-13 00:11:19 +1000

    chore: clearer lines

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -25,6 +25,6 @@ poetry run datasette publish vercel \
   --setting default_page_size 100 \
   --setting allow_csv_stream off \
   --token $VERCEL_TOKEN &&
-echo "CF" &&
+echo "\n\nCF\n\n" &&
 CLOUDFLARE_API_KEY=$CLOUDFLARE_PURGE_API_TOKEN poetry run \
   cli4 --delete purge_everything=true /zones/:bf147aba15972081173a0047f501e229/purge_cache
