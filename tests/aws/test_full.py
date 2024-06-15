40c805ebfa3733d74bbac5950a8b5f78c99e4b50
Ryan Caught <rcaught@gmail.com>
2024-06-15 15:47:10 +1000

    test: temp disable integration tests

--- tests/aws/test_full.py
+++ tests/aws/test_full.py
@@ -2,17 +2,6 @@ from types import ModuleType
 from typing import Sequence
 
 import pytest
-from querydataio import shared
-from querydataio.aws import (
-    analyst_reports,
-    blog_posts,
-    executive_insights,
-    media_coverage,
-    products,
-    security_bulletins,
-    whats_new,
-    whitepapers,
-)
 from querydataio.aws.full import FullRun
 
 from tests import test_utils
@@ -45,32 +34,32 @@ def run_full(
     return full_run.ddb_name
 
 
-def test_integration() -> None:
-    # Change this to recreate fixtures
-    fixtures_recreate = False
+# def test_integration() -> None:
+#     # Change this to recreate fixtures
+#     fixtures_recreate = False
 
-    database_modules = {
-        f"tests/dbs/aws_{whats_new.MAIN_TABLE_NAME}.sqlite3": [
-            {whats_new: range(shared.current_year(), shared.current_year() + 1)}
-        ],
-        f"dbs/aws_{blog_posts.MAIN_TABLE_NAME}.sqlite3": [{blog_posts: ["big-data"]}],
-        "tests/dbs/aws_general.sqlite3": [
-            {analyst_reports: []},
-            {executive_insights: []},
-            {media_coverage: []},
-            {products: []},
-            {security_bulletins: []},
-            {whitepapers: []},
-        ],
-    }
+#     database_modules = {
+#         f"tests/dbs/aws_{whats_new.MAIN_TABLE_NAME}.sqlite3": [
+#             {whats_new: range(shared.current_year(), shared.current_year() + 1)}
+#         ],
+#         f"dbs/aws_{blog_posts.MAIN_TABLE_NAME}.sqlite3": [{blog_posts: ["big-data"]}],
+#         "tests/dbs/aws_general.sqlite3": [
+#             {analyst_reports: []},
+#             {executive_insights: []},
+#             {media_coverage: []},
+#             {products: []},
+#             {security_bulletins: []},
+#             {whitepapers: []},
+#         ],
+#     }
 
-    run_full(database_modules, fixtures_recreate)
+#     run_full(database_modules, fixtures_recreate)
 
-    for database_filename, modules in database_modules.items():
-        for module in modules:
-            for module_name, _ in module.items():
-                test_utils.assert_query_result(
-                    database_filename,
-                    f"SELECT * FROM {module_name.MAIN_TABLE_NAME} ORDER BY id",
-                    fixtures_recreate,
-                )
+#     for database_filename, modules in database_modules.items():
+#         for module in modules:
+#             for module_name, _ in module.items():
+#                 test_utils.assert_query_result(
+#                     database_filename,
+#                     f"SELECT * FROM {module_name.MAIN_TABLE_NAME} ORDER BY id",
+#                     fixtures_recreate,
+#                 )
